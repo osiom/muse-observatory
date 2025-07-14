99f9b7e552080e2d0f2bdd653903457335cb372e
osiomatt <mos@hellofresh.com>
2025-07-14 10:36:06 +0200

    fix: intro message

--- app.py
+++ app.py
@@ -303,19 +303,19 @@ def main():
         ui.element("div").classes("noise")
     with ui.column().classes("centered-container"):
         with ui.element("div").classes("terminal"):
-            status = ui.html("muse-observatory init |").classes("init-text")
+            status = ui.html("muse-observatory initialisation |").classes("init-text")
         prompt = ui.label("Press Enter.").classes("prompt").style("opacity: 0")
         ui.html('<div class="telescope">🔭</div>')
 
     async def animate_loading():
         await asyncio.sleep(0.6)
-        status.set_content("muse-observatory init. ")
+        status.set_content("muse-observatory initialisation ")
         await asyncio.sleep(0.6)
-        status.set_content("muse-observatory init.. |")
+        status.set_content("muse-observatory initialisation.. |")
         await asyncio.sleep(0.6)
-        status.set_content("muse-observatory init...")
+        status.set_content("muse-observatory initialisation...")
         await asyncio.sleep(0.6)
-        status.set_content("muse-observatory init... |")
+        status.set_content("muse-observatory initialisation... |")
         await asyncio.sleep(0.6)
         status.set_content("loading completed.")
         await asyncio.sleep(0.7)
