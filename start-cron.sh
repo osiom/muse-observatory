2d07a46e69d0d046eb0b0fae13d5d65c31167eb3
osiomatt <mos@hellofresh.com>
2025-07-25 12:25:09 +0200

    fix: scheduler

--- start-cron.sh
+++ start-cron.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-# Activate venv
-. /venv/bin/activate
-
-# Setup cron job
-mkdir -p /etc/crontabs
-echo "1 0 * * * /venv/bin/python /app/generate_fact.py && docker restart muse-observatory-app" > /etc/crontabs/root
-
-# Show crontab for debug
-cat /etc/crontabs/root
-
-# Run cron in foreground with verbose logs
+# Just run cron in foreground — crontab is already set
 crond -f -l 8
