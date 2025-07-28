38c79a0b1e6efb5a9b097453e077642668fc2a51
osiom <matteo.osio1992@gmail.com>
2025-07-28 14:18:51 +0200

    fix: Schedule and cronjob

--- start-cron.sh
+++ start-cron.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
-# Activate venv
-. /venv/bin/activate
-
-# Setup cron job
-mkdir -p /etc/crontabs
-echo "1 0 * * * /venv/bin/python /app/generate_fact.py && docker restart muse-observatory-app" > /etc/crontabs/root
-
-# Show crontab for debug
-cat /etc/crontabs/root
-
-# Run cron in foreground with verbose logs
-crond -f -l 8
+#!/bin/bash
+source /venv/bin/activate
+/venv/bin/python /app/generate_fact.py
